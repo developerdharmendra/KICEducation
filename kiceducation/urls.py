267d3d9542c87b938ca2f44ab6084a524cd09744
Arpan Ingnam <ingnamarpan@gmail.com>
2025-11-11 20:08:44 +0545

    add: setup global static files

--- kiceducation/urls.py
+++ kiceducation/urls.py
@@ -14,6 +14,8 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
@@ -23,3 +25,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('kic.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
