59956ed726477ac4954f724b7f763ec2d10616e5
Arpan Ingnam <ingnamarpan@gmail.com>
2025-11-11 21:26:33 +0545

    setup accounts app

--- kiceducation/settings.py
+++ kiceducation/settings.py
@@ -37,7 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # local apps
+    'accounts',
     'kic',
+    # third-party packages
 ]
 
 MIDDLEWARE = [
@@ -129,6 +132,10 @@ MEDIA_ROOT = BASE_DIR / 'media'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+# custom user model configuration
+AUTH_USER_MODEL = 'accounts.CustomUser'
 
 
 # logging configuration
