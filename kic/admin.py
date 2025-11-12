c8695570865a4cffba024314ebb2d094895251bf
Arpan Ingnam <ingnamarpan@gmail.com>
2025-11-12 23:35:58 +0545

    add: setup model admin classes

--- kic/admin.py
+++ kic/admin.py
@@ -1,3 +1,68 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import (
+    Achievement,
+    Country,
+    Mission,
+    Service,
+    Testimonial,
+    TestPreparationClass,
+    University,
+)
+
+
+@admin.register(Achievement)
+class AchievementAdmin(admin.ModelAdmin):
+    list_display = ['title', 'date', 'icon']
+    search_fields = ['title']
+
+
+@admin.register(Country)
+class CountryAdmin(admin.ModelAdmin):
+    list_display = ['name', 'slug', 'flag']
+    readonly_fields = ['slug']
+    search_fields = ['name']
+
+
+@admin.register(Mission)
+class MissionAdmin(admin.ModelAdmin):
+    list_display = ['title', 'image']
+    search_fields = ['name']
+
+
+@admin.register(Service)
+class ServiceAdmin(admin.ModelAdmin):
+    list_display = ['name', 'slug', 'image']
+    readonly_fields = ['slug']
+    search_fields = ['name']
+
+
+@admin.register(Testimonial)
+class TestimonialAdmin(admin.ModelAdmin):
+    list_display = ['full_name', 'display_order', 'created_at', 'updated_at']
+    search_fields = ['full_name']
+
+
+@admin.register(TestPreparationClass)
+class TestPreparationClassAdmin(admin.ModelAdmin):
+    list_display = ['name', 'slug']
+    readonly_fields = ['slug']
+    search_fields = ['name']
+
+
+@admin.register(University)
+class UniversityAdmin(admin.ModelAdmin):
+    list_display = [
+        'name',
+        'slug',
+        'website',
+        'established_date',
+        'enrollment_season',
+        'ranking',
+        'country',
+        'status',
+        'is_featured',
+    ]
+    readonly_fields = ['slug']
+    search_fields = ['name', 'country__name']
+    list_filter = ['status', 'is_featured']
