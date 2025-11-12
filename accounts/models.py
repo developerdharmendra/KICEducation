d382823d27bf63eb5c7cfb05b0b45e6105ff1900
Arpan Ingnam <ingnamarpan@gmail.com>
2025-11-12 21:38:40 +0545

    setup email field as USERNAME_FIELD

--- accounts/models.py
+++ accounts/models.py
@@ -3,5 +3,10 @@ from django.db import models
 
 
 class CustomUser(AbstractUser):
-    pass
+    email = models.EmailField(unique=True)
 
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['username']
+
+    def __str__(self):
+        return self.email
